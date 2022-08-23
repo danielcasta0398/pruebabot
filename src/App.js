641aa297794a592e1197fa2b23c7d7180145ca5b
danielcasta0398 <98791147+danielcasta0398@users.noreply.github.com>
2022-08-23 17:29:59 +0200

    sds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { io } from "socket.io-client";
+
 
 function App() {
+
+  const socket = io("https://01b2-2a0c-5a81-5307-a500-38e5-bf57-e983-4634.ngrok.io/");
+
+  socket.on("connect", () => {
+    console.log(socket.connected); // true
+  });
+
+  socket.on('hola', data => console.log(data))
+
   return (
     <div className="App">
       <header className="App-header">
